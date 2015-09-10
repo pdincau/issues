af1dfff808d52265af8856a1a4fe72091f9fa3d1
Paolo D'Incau <paolo.dincau@xpeppers.com>
2015-09-10 15:26:46 +0200

    Adding method run/1 to wrap parse_args

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -2,6 +2,10 @@ defmodule Issues.CLI do
 
   @default_count 4
 
+  def run(argv) do
+    parse_args(argv)
+  end
+
   def parse_args(argv) do
     parse = OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
     case parse do
